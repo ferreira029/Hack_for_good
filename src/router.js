b94905782a4a70691f46cac621fbef3031827345
Bruno <Brsant_2@hotmail.com>
2020-04-02 11:19:47 -0300

    fix conflicts of the code

--- src/router.js
+++ src/router.js
@@ -1,9 +1,5 @@
 const express = require('express');
-<<<<<<< HEAD
-const globo =  require('./functions/g1.js');
-=======
 const generalFunction =  require('./functions/generalFunction');
->>>>>>> e9dd52b7c220b70ee5ee3d278fdaee4521256895
 
 const router = express();
 
