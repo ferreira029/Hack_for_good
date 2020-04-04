f43d88d25be2a0d4be457efd734ed51d321143fe
Bruno <Brsant_2@hotmail.com>
2020-04-04 12:53:34 -0300

    Search in Ministério da saúde

--- src/functions/generalFunction.js
+++ src/functions/generalFunction.js
@@ -13,20 +13,20 @@ module.exports = {
     async index(req, res) {
 
         const userData = {
-            text: "Corona em são paulo".toUpperCase(),
+            text: "O novo agente do coronavírus foi descoberto em 31/12/19 após casos registrados na China teste".toUpperCase(),
             search: "afojfoafpwqpwkqpofwnfqpwf wfssfas fs´mafpqf Corona em são paUlo nasnfonfa ijoasf-afs".toUpperCase()
         };
 
         try {
-
+            let search = "corona ministerio da saude";
             // Faz a pequisa na API do google que retorna o JSON com os links
-            const response = await axios.get('https://www.googleapis.com/customsearch/v1?key=AIzaSyDECtd0EdDZv3Tr85q8Vv7i-126gHSiOQI&cx=017364834723098479907:hiqtvhyt05s&q=Corona&fields=items(link)').catch(err => console.log(`API DO GOOGLE NÃO FUNCIONA, ${err}`));
+            const response = await axios.get(`https://www.googleapis.com/customsearch/v1/siterestrict?key=AIzaSyDECtd0EdDZv3Tr85q8Vv7i-126gHSiOQI&cx=017232608039431587026:2zyvwylqmhq&q=${search}&fields=items(link)`).catch(err => console.log(`API DO GOOGLE NÃO FUNCIONA, ${err}`));
             let findContent = '';
-            let file = '';
+            //let file = '';
 
             // Pega os itens do JSON e transforma em array
             Object.entries(response.data.items).map(async resp => {
-                //console.log(resp[1].link);
+                console.log(resp[1].link);
 
                 // Verifica de qual site se trata o link e preenche as variáveis
                 if (resp[1].link.includes('https://www.uol.com.br')) {
@@ -40,32 +40,36 @@ module.exports = {
                     file = 'Saude';
                 }
 
-                // Chamada assíncrona para preencher os arquivos de texto com o conteúdo das páginas da pesquisa
+                // Chamada assíncrona para preencher o conteúdo das páginas da pesquisa
                 await request(resp[1].link, function (err, res, body) {
                     if (err) console.log(`Erro: ${err}`);
 
                     let $ = cheerio.load(body);
 
                     // Apaga o arquivo de texto
-                    fs.truncate(`${path.resolve(`src/data/${file}${resp[0]}.txt`)}`, () => { return; });
+                    //fs.truncate(`${path.resolve(`src/data/${file}${resp[0]}.txt`)}`, () => { return; });
 
                     // Pega o conteudo dos 'p' das páginas
                     $(findContent).each(function () {
-                        let content = $(this).find('p').text().trim();
-
+                        let content = $(this).find('p').text().trim().toUpperCase();
                         if (content == "") {
                             return;
                         } else {
-                            // Cria/preenche o arquivo
+                            // Cria/preenche o conteudo
                             let arrayUser = userData.text.split(" ");
 
                             for (i = 0; i < arrayUser.length; i++) {
                                 let position;
-                                position = userData.search.search(arrayUser[i]);
+                                //console.log(arrayUser[i]);
+                                position = content.search(arrayUser[i]);
+                                // Debug improvisado
+                                //console.log("\nSite pesquisado: " + resp[1].link + "\nConteudo:" + content + "\nO que foi procurado:" + arrayUser[i] +
+                                //"\nA posição da procura é: " + position + " no contetudo");
 
                                 if (position > -1) {
                                     // Retorna o texto cortado com o a primeira palavra da pesquisa
-                                    let cut = userData.search.slice(position, userData.search.length);
+                                    let cut = content.slice(position, content.length);
+                                    //console.log(cut);
 
                                     // <--- Começa o segundo corte da pesquisa --->
                                     for (j = arrayUser.length - 1; j > 0; j--) {
@@ -85,21 +89,37 @@ module.exports = {
                                             //console.log(final);
                                             percent = levenshtein.levenshtein(userData.text.toUpperCase(), final.toUpperCase());
 
-                                            // Apresenta a porcentagem de veracidade da pesquisa
-                                            console.log(`A informação: "${final}" é ${percent}% verídica`);
-                                            return true;
+                                            // Apresenta a resposta se a porcentagem de veracidade da pesquisa for maior que 60%
+
+                                            /* if(percent > 60) {
+                                                console.log("\nSite pesquisado: " + resp[1].link + "\nO que foi procurado:" + arrayUser[i] +
+                                            "\nA posição da procura é: " + position + " no contetudo" + 
+                                                `\nConteudo: ${content}\nInformação: ${final}\nA pesquisa: "${userData.text}" é ${percent}% verídica`);
+                                                return true;
+                                            } else {
+                                                return false;
+                                            } */
+                                            if(percent > 60) {
+                                            percent > 60 ? console.log("\nSite pesquisado: " + resp[1].link + "\nO que foi procurado:" + arrayUser[i] +
+                                            "\nA posição da procura é: " + position + " no contetudo" + 
+                                                `\nConteudo: ${content}\nInformação: ${final}\nA pesquisa: "${userData.text}" é ${percent}% verídica`): false;
+                                                return true;
+                                            }
+                                            
+                                            /* percent > 60 ? position2 = -2 : false;
+                                            console.log("Position2: " + position2); */
                                         }
+                                        /* percent > 60 ? j = -1 : false;
+                                        console.log("j: " + j); */
                                     }
-
-                                    return true;
+                                    /* percent > 60 ? position = -2 : false;
+                                    console.log("Position: " + position2); */
                                 }
-
-                                //percent >= 60 ? true : false;
-
+                                /* percent > 60 ? i = arrayUser.length+1 : false;
+                                console.log("i: " + i); */
                                 // Ver em qual posição do texto está a pesquisa
                                 //console.log(`${arrayUser[i]} está na posição: ${userData.pesq.search(arrayUser[i])} do texto`);
                             }
-
                             // Criar arquivo de texto
                             //fs.appendFile(`${path.resolve(`../data/${file}${resp[0]}.txt`)}`, `${content} \n`, () => { return;});
                         }
