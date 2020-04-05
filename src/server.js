374d336fa837566048b2f584c62d170ae55902e3
Marc Ferreira <msoftware.marc@outlook.com>
2020-04-05 18:18:05 -0300

    Fix CORS of the our code

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const routes = require('./router');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 // Servidor vai aceitar requisições via JSON
 app.use(express.json());
 
