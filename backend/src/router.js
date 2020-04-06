8abc37c5dafe833e84867d015882a0909a0fefda
Marc Ferreira <msoftware.marc@outlook.com>
2020-04-06 10:26:05 -0300

    Create database connection

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const generalFunction =  require('./functions/generalFunction');
+const userController = require('./controllers/userController');
+const loginController = require('./controllers/loginController');
 
 const router = express();
 
 router.post('/search', generalFunction.index);
+
+router.post('/users', userController.create);
+router.get('/users', userController.index);
+router.post('/login', loginController.create);
 
 // Tornar a variável visivel em outros arquivos
 module.exports = router;
